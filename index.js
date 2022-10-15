a97efc1750481ad0775e0c9cfa5373e4e980bbfa
Ontaria Woods <ontaria.woods@gmail.com>
2022-10-15 16:10:40 -0400

    commit tree 7af0f9c83a398246677bd8ca0c131f42689c3477

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const firstTwo = ['Antonia', 'Nuru']
+const lastTwo = ['Amari', 'Mo']
+
+const returnFirstTwoDrivers = function () {
+    return firstTwo
+}
+const returnLastTwoDrivers = function () {
+    return lastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+    return function () {
+        return x * x
+    }
+}
+
+let fareDoubler = function (fare) {
+    return fare * 2
+}
+
+let fareTripler = function (fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, lastTwo) {
+    if (lastTwo === returnFirstTwoDrivers) {
+        return ['Antonia', 'Nuru']
+    } else if (lastTwo === returnLastTwoDrivers) {
+        return ['Amari', 'Mo']
+    }
+}
+
+
